09100a5c4e72a213fe1483abac4b2e61b923be84
Sasi <sasi.govindar@gmail.com>
2019-10-08 11:28:25 +0200

    NavBar added

---
+++ src/components/NavBar.vue
@@ -0,0 +1,18 @@
+<template>
+    <nav>
+        <v-toolbar app>
+            <v-spacer></v-spacer>
+            <v-toolbar-title class="text-uppercase black--text">
+                <span class="font-weight-light font-italic">Flash</span>
+                <span>Cards</span>
+            </v-toolbar-title>
+            <v-spacer></v-spacer>
+        </v-toolbar>
+    </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
