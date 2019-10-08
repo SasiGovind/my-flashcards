3db49a20a8f87f6258c04b377f5a282ccf6383bd
Sasi <sasi.govindar@gmail.com>
2019-10-08 11:28:25 +0200

    NavBar added

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,20 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app class="green ligthen-4">
+    <NavBar/>
+    <v-content>
+      <router-link to="/">Home</router-link>
+      <router-link to="/login">Login</router-link>
+      <router-link to="/settings">Settings</router-link>
+      <router-view/>
+    </v-content>
+  </v-app>
 </template>
+
+<script>
+import NavBar from '@/components/NavBar'
+
+export default {
+  name: 'App',
+  components: { NavBar }
+}
+</script>
