b69ef2609895dda4d29c243388cf16429cd43a3a
Sasi <sasi.govindar@gmail.com>
2019-10-12 16:32:41 +0200

    Gestion des docks/decks

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,9 @@
   <v-app class="green ligthen-4">
     <NavBar/>
     <v-content>
-      <router-link to="/">Home</router-link>
+      <!-- <router-link to="/">Home</router-link>
       <router-link to="/login">Login</router-link>
-      <router-link to="/settings">Settings</router-link>
+      <router-link to="/settings">Settings</router-link> -->
       <router-view/>
     </v-content>
   </v-app>
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,18 +1,19 @@
 <template>
-    <nav>
-        <v-toolbar app>
-            <v-spacer></v-spacer>
-            <v-toolbar-title class="text-uppercase black--text">
-                <span class="font-weight-light font-italic">Flash</span>
-                <span>Cards</span>
-            </v-toolbar-title>
-            <v-spacer></v-spacer>
-        </v-toolbar>
-    </nav>
+  <nav>
+    <v-toolbar app extended src='https://dl3.pushbulletusercontent.com/oIFtL0xN9dCae5vtAodbLprQXiDTSpce/121755_original_4000x3000.jpg' extensionHeight=''>
+      <v-spacer></v-spacer>
+      <v-toolbar-title class="text-uppercase black--text display-2">
+        <span class="font-weight-light font-italic">Flash</span>
+        <span>Cards</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn icon>
+        <v-icon>mdi-export</v-icon>
+      </v-btn>
+    </v-toolbar>
+  </nav>
 </template>
 
 <script>
-export default {
-
-}
+export default {}
 </script>
