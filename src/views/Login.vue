64ff07585801a9aa7b1ec3e11cafb923aa11444f
Shinui <denis.ulrick@hotmail.fr>
2019-10-13 19:29:09 +0200

    Formulaire lien server.js

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,15 +1,16 @@
 <template>
   <v-form id="form" ref="form" v-model="valid" lazy-validation>
     <v-col cols="12" sm="6">
-    <v-text-field v-model="name" :counter="12" :rules="idRules" label="Identifiant" required></v-text-field>
+    <v-text-field v-model="name" ref="id" :counter="12" :rules="idRules" label="Identifiant" required></v-text-field>
    
-    <v-text-field v-model="password" :append-icon="show ? 'visibility' : 'visibility_off'"
-            :rules="[rulesPWD.required, rulesPWD.min]" :type="show ? 'text' : 'password'"  counter
-            @click:append="show = !show"  label="Mot de passe" required>
+    <v-text-field v-model="password" :append-icon="show ? 'mdi-plus' : 'mdi-visibility_off'"
+            :rules="[rulesPWD.required, rulesPWD.min]" :type="show ? 'text' : 'password'" 
+             counter
+            @click:append="show = !show" ref="mdp" label="Mot de passe" required>
     </v-text-field>
     </v-col>
     
-    <v-btn :disabled="!valid" color="success" class="mr-4" @click="login">valider</v-btn>
+    <v-btn :disabled="!valid" color="success" class="mr-4" @click="login()">valider</v-btn>
 
     <v-btn color="error" class="mr-4" @click="reset">Reset</v-btn>
 
@@ -20,13 +21,15 @@
 export default {
   data: () => ({
     valid: true,
+    name: '',
+    password: '',
     id: "",
     url: "http://localhost:4000",
     idRules: [
       v => !!v || "Identifiant requis",
       v =>
         (v && v.length <= 12) ||
-        'L"Identifant doit faire au plus 12 caracteres'
+        "L'Identifant doit faire au plus 12 caracteres"
     ],
     show: false,
     rulesPWD: {
@@ -38,14 +41,23 @@ export default {
   methods: {
     async login() {
       // connecter l'utilisateur
-      alert("test");
-      if (this.$refs.form.validate()) {
-        this.snackbar = true;
-      }
+
       const response = await this.axios.post(this.url + "/api/login", {
-        login: v,
-        password: password,
-      });
+        login: this.name ,
+        password: this.password,
+      }); 
+
+      alert("Le message est " + response.data.message)
+
+      if (response.data.message === 'creation') {
+        alert("Compte créé ! Vous avez été connecté à votre compte " + this.name +" !");
+        this.$router.push('/')
+      }
+
+       if (response.data.message === 'connected'){
+        this.$router.push('/')
+        alert("Vous avez été connecté à votre compte " + this.name +" !");
+      }
       console.log("response is:", response);
     },
     logout() {},
