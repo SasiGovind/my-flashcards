c844522f9d43241f77fcc4e4e6c31c2e47f3e3e2
antho <anthof94@gmail.com>
2019-10-16 21:49:52 +0200

    ajout du footer provisoire

--- src/App.vue
+++ src/App.vue
@@ -7,14 +7,27 @@
       <router-link to="/settings">Settings</router-link> -->
       <router-view/>
     </v-content>
+    <footer><baseLayout/></footer>
   </v-app>
 </template>
 
+<style>
+footer {
+  color: white;
+  background:rgb(56, 122, 243);
+  position: absolute;
+  bottom: 0;
+  padding: 30px;
+  width: 100%
+}
+</style>
+
 <script>
 import NavBar from '@/components/NavBar'
+import baseLayout from '@/components/baseLayout'
 
 export default {
   name: 'App',
-  components: { NavBar }
+  components: { NavBar, baseLayout }
 }
 </script>
---
+++ src/components/baseLayout.vue
@@ -0,0 +1,37 @@
+
+<template #footer>
+    <div>
+        <p>
+            <strong>
+                <span style="float:left">Nous contacter</span>
+            </strong>
+            <br><br>
+            <strong>
+                <span style="float:left">Notre équipe</span>
+                <span style="float:right">Date: {{date}}</span>
+            </strong>
+            <br><br>
+            <strong>
+                <span style="float:left">Confidentialité</span>
+                <span style="float:right">Langue sélectionnée:</span>
+            </strong>
+            <select v-model="langues" style="background:white; float:right">
+                <option disabled value="">Choisir</option>
+                <option>Français</option>
+                <option>Anglais</option>
+            </select>
+        </p>
+    </div>
+</template>
+<!--</baseLayout>-->
+
+<script>
+export default {
+    data: function() {
+        return {
+            date: new Date().toLocaleString(),
+            langues: []
+        }
+    }
+}
+</script>
