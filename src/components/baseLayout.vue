1cc85ad0e24b23362758e7412f45c89246cfd485
antho <anthof94@gmail.com>
2019-10-26 17:43:58 +0200

    footer moins large

--- src/components/baseLayout.vue
+++ src/components/baseLayout.vue
@@ -1,20 +1,16 @@
-<template #footer>
-<footer>
-    <strong>
-        <span style="float:left">Nous contacter</span>
-        <br><br>
-        <span style="float:left">Notre équipe</span>
-        <span style="float:right">Date: {{date}}</span>
-    <br><br>
-        <span style="float:left">Confidentialité</span>
-        <span style="float:right">Langue sélectionnée: 
-            <select v-model="langues" style="background:white">
-                <option disabled value="">Choisir</option>
-                <option>Français</option>
-                <option>Anglais</option>
-            </select>
-        </span>
-    </strong>
+<template>
+<footer style="display:flex">
+    <span>Mis en ligne le 07/10/2019</span>
+    <a v-for="tag in tags" :href="tag.lien" style="color:white; text-decoration: none">
+        {{ tag.libelle }}
+    </a>
+    <span>Langue sélectionnée:
+        <select v-model="langues" style="color: black; font-weight: normal; background:white; appearance: menulist">
+            <option disabled value=''>Choisir</option>
+            <option>Français</option>
+            <option>Anglais</option>
+        </select>
+    </span>
 </footer>
 </template>
 
@@ -24,18 +20,24 @@ footer {
   background:rgb(56, 122, 243);
   position: absolute;
   bottom: 0;
-  padding: 30px;
-  width: 100%
+  padding: 15px;
+  width: 100%;
+  font-weight:bold;
+  justify-content: space-around;
 }
 </style>
 
 <script>
 export default {
-    data: function() {
-        return {
-            date: new Date().toLocaleString(),
-            langues: []
-        }
+  data: function () {
+    return {
+      tags: [
+        { lien: 'https://www.google.fr', libelle: 'Qui sommes nous?' },
+        { lien: 'https://www.google.fr', libelle: 'Mentions légales' },
+        { lien: 'https://www.google.fr', libelle: 'Contact' }
+      ],
+      langues: ''
     }
+  }
 }
 </script>
