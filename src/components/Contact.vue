5352d06a1358acde739085a569529812450cd3d2
antho <anthof94@gmail.com>
2019-11-12 00:06:04 +0100

    mail automatique et syntaxe verifiee

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -4,9 +4,10 @@
             <a style="color:white;" v-on="on">{{ textes[no_langue][0] }}</a>
         </template>
         <v-card>
-            <v-card-title>{{ textes[no_langue][0] }}</v-card-title>
-            <v-card-text>
-                <v-row>
+            <v-form ref="form" v-model="valid" lazy-validation>
+              <v-card-title>{{ textes[no_langue][0] }}</v-card-title>
+              <v-card-text>
+                  <v-row>
                     <v-col md="6">
                     <v-text-field :label=textes[no_langue][1] v-model="message.nom"/>
                     </v-col>
@@ -14,19 +15,20 @@
                     <v-text-field :label=textes[no_langue][2] v-model="message.prenom"/>
                     </v-col>
                     <v-col>
-                        <v-text-field :label=textes[no_langue][3] :hint=textes[no_langue][4] v-model="message.from"/>
+                        <v-text-field :label=textes[no_langue][3] :hint=textes[no_langue][4] :rules="rulesMail" v-model="message.from"/>
                     </v-col>
-                </v-row>
-                <v-col>
-                    <v-textarea :label=textes[no_langue][5] v-model="message.text"/>
-                </v-col>
-            </v-card-text>
-            <v-card-actions class="justify-center">
-                <v-btn color="blue" @click="verif">
-                    {{ textes[no_langue][6] }}
-                </v-btn>
-                <br><br>
-            </v-card-actions>
+                  </v-row>
+                  <v-col>
+                      <v-textarea :label=textes[no_langue][5] :rules="rulesMsg" v-model="message.text"/>
+                  </v-col>
+              </v-card-text>
+              <v-card-actions class="justify-center">
+                  <v-btn color="blue" @click="verif" :disabled="!valid">
+                      {{ textes[no_langue][6] }}
+                  </v-btn>
+                  <br><br>
+              </v-card-actions>
+            </v-form>
         </v-card>
     </v-dialog>
 </template>
@@ -37,12 +39,16 @@ export default {
   data () {
     return {
       dialog: false,
+      valid: true,
       message: {
         nom: '',
         prenom: '',
         from: '',
+        to: '',
         text: ''
       },
+      rulesMail: [0],
+      rulesMsg: [0],
       textes: [
         [
           'Nous contacter',
@@ -52,7 +58,9 @@ export default {
           'Pour que nous puissions vous répondre!',
           'Texte (champ obligatoire)',
           'Envoyer',
-          'Valider votre saisie?'
+          'Valider votre saisie?',
+          "L'adresse email doit être valide",
+          'Le message est vide!'
         ],
         [
           'Contact us',
@@ -62,21 +70,35 @@ export default {
           'So that we can answer you!',
           'Text (required field)',
           'Send',
-          'Validate your entry?'
+          'Validate your entry?',
+          'E-mail must be valid',
+          'The message is empty!'
         ]
       ]
     }
   },
   methods: {
     async verif () {
-      if (this.message.text !== '') {
+      if (this.$refs.form.validate()) {
         if (confirm(this.textes[this.no_langue][7])) {
           this.message.subject = 'Contact ' + this.message.nom + ' ' + this.message.prenom
-          const response = await this.axios.post('http://localhost:4000/api/mail', this.message)
+          const response = await this.axios.post('http://localhost:' + process.env.PORT_SERVER +'/api/mail', this.message)
           console.log('reponse', response)
         }
       }
+    },
+    set_rules () {
+      this.rulesMail[0] = val => (!val || /.+@.+\..+/.test(val)) || this.textes[this.no_langue][8]
+      this.rulesMsg[0] = v => (v && v.length > 0) || this.textes[this.no_langue][9]
     }
+  },
+  watch: {
+    no_langue: function () {
+      this.set_rules()
+    }
+  },
+  created: function () {
+    this.set_rules()
   }
 }
 </script>
